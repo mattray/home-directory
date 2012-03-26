6b3663ec9cd60cd85a50b09a5959858812007461
mattray <matthewhray@gmail.com>
2012-03-26 16:33:06 -0400

    updated example

--- notes/tmux.sh
+++ notes/tmux.sh
@@ -1,3 +1,14 @@
+# Ctrl-b c Create new window
+# Ctrl-b d Detach current client
+# Ctrl-b l Move to previously selected window
+# Ctrl-b n Move to the next window
+# Ctrl-b p Move to the previous window
+# Ctrl-b & Kill the current window
+# Ctrl-b , Rename the current window
+# Ctrl-b % Split the current window into two panes
+# Ctrl-b q Show pane numbers (used to switch between panes)
+# Ctrl-b o Switch to the next pane
+# Ctrl-b ? List all keybindings
 tmux start-server
 tmux new-session -d -s morbo -n "#0"
 tmux new-window -t morbo:1 -n "#1"
@@ -9,35 +20,35 @@ tmux new-window -t morbo:6 -n "#6"
 tmux new-window -t morbo:7 -n "#7"
 tmux new-window -t morbo:8 -n "#8"
 tmux new-window -t morbo:9 -n "#9"
-tmux send-keys -t morbo:0 "rvm system" C-m
-tmux send-keys -t morbo:1 "rvm system" C-m
-tmux send-keys -t morbo:2 "rvm system" C-m
-tmux send-keys -t morbo:3 "rvm system" C-m
-tmux send-keys -t morbo:4 "rvm system" C-m
-tmux send-keys -t morbo:5 "rvm system" C-m
-tmux send-keys -t morbo:6 "rvm system" C-m
-tmux send-keys -t morbo:7 "rvm system" C-m
-tmux send-keys -t morbo:8 "rvm system" C-m
-tmux send-keys -t morbo:9 "rvm system" C-m
-tmux send-keys -t morbo:0 "cd ~/ws/chef-repo" C-m
-tmux send-keys -t morbo:1 "cd ~/ws/chef-repo" C-m
-tmux send-keys -t morbo:2 "cd ~/ws/chef-repo" C-m
-tmux send-keys -t morbo:3 "cd ~/ws/chef-repo" C-m
-tmux send-keys -t morbo:4 "cd ~/ws/chef-repo" C-m
-tmux send-keys -t morbo:5 "cd ~/ws/chef-repo" C-m
-tmux send-keys -t morbo:6 "cd ~/ws/chef-repo" C-m
-tmux send-keys -t morbo:7 "cd ~/ws/chef-repo" C-m
-tmux send-keys -t morbo:8 "cd ~/ws/chef-repo" C-m
-tmux send-keys -t morbo:9 "cd ~/ws/chef-repo" C-m
+tmux send-keys -t morbo:0 "rvm 1.9.2" C-m
+tmux send-keys -t morbo:1 "rvm 1.9.2" C-m
+tmux send-keys -t morbo:2 "rvm 1.9.2" C-m
+tmux send-keys -t morbo:3 "rvm 1.9.2" C-m
+tmux send-keys -t morbo:4 "rvm 1.9.2" C-m
+tmux send-keys -t morbo:5 "rvm 1.9.2" C-m
+tmux send-keys -t morbo:6 "rvm 1.9.2" C-m
+tmux send-keys -t morbo:7 "rvm 1.9.2" C-m
+tmux send-keys -t morbo:8 "rvm 1.9.2" C-m
+tmux send-keys -t morbo:9 "rvm 1.9.2" C-m
+tmux send-keys -t morbo:0 "cd ~/ws/demo-repo" C-m
+tmux send-keys -t morbo:1 "cd ~/ws/demo-repo" C-m
+tmux send-keys -t morbo:2 "cd ~/ws/demo-repo" C-m
+tmux send-keys -t morbo:3 "cd ~/ws/demo-repo" C-m
+tmux send-keys -t morbo:4 "cd ~/ws/demo-repo" C-m
+tmux send-keys -t morbo:5 "cd ~/ws/demo-repo" C-m
+tmux send-keys -t morbo:6 "cd ~/ws/demo-repo" C-m
+tmux send-keys -t morbo:7 "cd ~/ws/demo-repo" C-m
+tmux send-keys -t morbo:8 "cd ~/ws/demo-repo" C-m
+tmux send-keys -t morbo:9 "cd ~/ws/demo-repo" C-m
 sleep 10
-tmux send-keys -t morbo:0 "knife ec2 server create 'role[Seattle]' 'role[ServerSSHLinuxMySQL]' -S mray -I ~/.ssh/mray.pem  -x ubuntu -G default -i ami-a2f405cb -f t1.micro" C-m
-tmux send-keys -t morbo:1 "knife ec2 server create 'role[base]' 'role[Austin]' 'role[ServerSSHLinux]' -S mray -I ~/.ssh/mray.pem  -x ubuntu -G default -i ami-a2f405cb -f t1.micro" C-m
-tmux send-keys -t morbo:2 "knife ec2 server create 'role[base]' 'role[Austin]' 'role[ServerSSHLinux]' -S mray -I ~/.ssh/mray.pem  -x ubuntu -G default -i ami-a2f405cb -f t1.micro" C-m
-tmux send-keys -t morbo:3 "knife ec2 server create 'role[base]' 'role[Austin]' 'role[ServerSSHLinux]' -S mray -I ~/.ssh/mray.pem  -x ubuntu -G default -i ami-a2f405cb -f t1.micro" C-m
-tmux send-keys -t morbo:4 "knife ec2 server create 'role[Austin]' 'role[ServerSSHLinux]' -S mray -I ~/.ssh/mray.pem  -x ubuntu -G default -i ami-a2f405cb -f t1.micro" C-m
-tmux send-keys -t morbo:5 "knife ec2 server create 'role[Seattle]' 'role[ServerSSHLinux]' -S mray -I ~/.ssh/mray.pem  -x ubuntu -G default -i ami-a2f405cb -f t1.micro" C-m
-tmux send-keys -t morbo:6 "knife ec2 server create 'role[Seattle]' 'role[ServerSSHLinux]' -S mray -I ~/.ssh/mray.pem  -x ubuntu -G default -i ami-a2f405cb -f t1.micro" C-m
-tmux send-keys -t morbo:7 "knife ec2 server create 'role[base]' 'role[Seattle]' 'recipe[zenoss::client]' -S mray -I ~/.ssh/mray.pem  -x ubuntu -G default -i ami-a2f405cb -f t1.micro" C-m
-tmux send-keys -t morbo:8 "knife ec2 server create 'role[base]' 'role[Seattle]' 'recipe[zenoss::client]' -S mray -I ~/.ssh/mray.pem  -x ubuntu -G default -i ami-a2f405cb -f t1.micro" C-m
-tmux send-keys -t morbo:9 "knife ec2 server create 'role[base]' 'role[Seattle]' 'role[ServerSSHLinuxMySQL]' -S mray -I ~/.ssh/mray.pem  -x ubuntu -G default -i ami-a2f405cb -f t1.micro" C-m
+tmux send-keys -t morbo:0 "knife ec2 server create -S mray -i ~/.ssh/mray.pem -x ubuntu -G default -I ami-a7a97dce -f m1.small -d omnibus -r 'role[btpeer],recipe[wfdemo]'" C-m
+tmux send-keys -t morbo:1 "knife ec2 server create -S mray -i ~/.ssh/mray.pem -x ubuntu -G default -I ami-a7a97dce -f m1.small -d omnibus -r 'role[btpeer],recipe[wfdemo]'" C-m
+tmux send-keys -t morbo:2 "knife ec2 server create -S mray -i ~/.ssh/mray.pem -x ubuntu -G default -I ami-a7a97dce -f m1.small -d omnibus -r 'role[btpeer],recipe[wfdemo]'" C-m
+tmux send-keys -t morbo:3 "knife ec2 server create -S mray -i ~/.ssh/mray.pem -x ubuntu -G default -I ami-a7a97dce -f m1.small -d omnibus -r 'role[btpeer],recipe[wfdemo]'" C-m
+tmux send-keys -t morbo:4 "knife ec2 server create -S mray -i ~/.ssh/mray.pem -x ubuntu -G default -I ami-a7a97dce -f m1.small -d omnibus -r 'role[btpeer],recipe[wfdemo]'" C-m
+tmux send-keys -t morbo:5 "knife ec2 server create -S mray -i ~/.ssh/mray.pem -x ubuntu -G default -I ami-a7a97dce -f m1.small -d omnibus -r 'role[btpeer],recipe[wfdemo]'" C-m
+tmux send-keys -t morbo:6 "knife ec2 server create -S mray -i ~/.ssh/mray.pem -x ubuntu -G default -I ami-a7a97dce -f m1.small -d omnibus -r 'role[btpeer],recipe[wfdemo]'" C-m
+tmux send-keys -t morbo:7 "knife ec2 server create -S mray -i ~/.ssh/mray.pem -x ubuntu -G default -I ami-a7a97dce -f m1.small -d omnibus -r 'role[btpeer],recipe[wfdemo]'" C-m
+tmux send-keys -t morbo:8 "knife ec2 server create -S mray -i ~/.ssh/mray.pem -x ubuntu -G default -I ami-a7a97dce -f m1.small -d omnibus -r 'role[btpeer],recipe[wfdemo]'" C-m
+tmux send-keys -t morbo:9 "knife ec2 server create -S mray -i ~/.ssh/mray.pem -x ubuntu -G default -I ami-a7a97dce -f m1.small -d omnibus -r 'role[btpeer],recipe[wfdemo]'" C-m
 tmux attach-session -t morbo
