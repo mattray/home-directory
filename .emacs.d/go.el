b98e323d642818f0286a642c30fa03ef8c02bf66
Matt Ray <matthewhray@gmail.com>
2017-03-18 14:37:26 +1100

    update Go paths

--- .emacs.d/go.el
+++ .emacs.d/go.el
@@ -1,13 +1,13 @@
 ;; Go
 ;; https://github.com/dominikh/go-mode.el
 ;; go-mode via elpa
-(add-to-list 'load-path "~/.emacs.d/elpa/go-mode-20151226.1224/" t)
-(require 'go-mode-autoloads)
+(add-to-list 'load-path "~/.emacs.d/elpa/go-mode-20170308.1512/")
+;;(require 'go-mode-autoloads)
 (setenv "GOPATH" (getenv "GOPATH"))
 (setenv "PATH" (getenv "PATH"))
 (setq exec-path (append exec-path '("/usr/local/go/bin") '("/Users/mray/go/bin")))
 ;; https://github.com/syohex/emacs-go-eldoc
-(add-to-list 'load-path "~/.emacs.d/elpa/go-eldoc-20151028.10/" t)
+(add-to-list 'load-path "~/.emacs.d/elpa/go-eldoc-20170305.627/")
 (require 'go-eldoc)
 (add-hook 'go-mode-hook 'go-eldoc-setup)
 ;; go-mode customizations
