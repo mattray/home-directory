ea80e6ede59287bca7ec9a7d115b57e6b54c7f97
Matt Ray <github@mattray.dev>
2021-05-26 15:43:08 +1000

    Homebrew path

--- .emacs.d/flyspell.el
+++ .emacs.d/flyspell.el
@@ -1,6 +1,6 @@
 ;;; Flyspell
 (autoload 'flyspell-mode "flyspell" "On-the-fly spelling checker." t)
-(setq ispell-program-name "/usr/local/bin/ispell") ;; installed with brew
+(setq ispell-program-name "/opt/homebrew/bin/ispell") ;; installed with brew
 (defun turn-on-flyspell ()
   "Unconditionally turn on Flyspell mode."
   (flyspell-mode 1))
