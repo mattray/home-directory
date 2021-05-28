6aca5d28dfa520c87b0c9169dfbc87668f6d4c68
Matt Ray <github@mattray.dev>
2021-05-28 14:10:26 +1000

    not defaulting to bash

--- .emacs.d/tramp.el
+++ .emacs.d/tramp.el
@@ -1,5 +1,2 @@
-;; read this more later https://github.com/howardabrams/dot-files/blob/master/emacs-eshell.org
-
-(setq explicit-shell-file-name "/bin/bash")
 (setq tramp-histfile-override "/dev/null")
 (setenv "PAGER" "cat")
